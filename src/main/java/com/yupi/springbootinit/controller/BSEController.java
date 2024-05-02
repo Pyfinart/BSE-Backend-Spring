99e1070c28d2619c3d6b0f3603d48793ae4d5caa
pyfinart <mypyfinart@gmail.com>
2024-05-03 02:20:18 +0800

    function of validating parameter of algorithm on proceed 2nd

--- src/main/java/com/yupi/springbootinit/controller/BSEController.java
+++ src/main/java/com/yupi/springbootinit/controller/BSEController.java
@@ -2,6 +2,7 @@ package com.yupi.springbootinit.controller;
 
 import com.yupi.springbootinit.common.BaseResponse;
 import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.constant.BSEConstant;
 import com.yupi.springbootinit.model.dto.bse.BSEAlgorithmParameterRequest;
 import com.yupi.springbootinit.model.enums.CallbackState;
 import com.yupi.springbootinit.service.BSEService;
@@ -40,33 +41,4 @@ public class BSEController {
         }
     }
 
-    public void validateParameter(BSEAlgorithmParameterRequest bseAlgorithmParameterRequest){
-        // default value
-        // start_time 0
-        // end_time 600
-        // buyer_range (80, 310)
-        // buyer_range (250, 490)
-        if(bseAlgorithmParameterRequest.getStartTime() < 0) bseAlgorithmParameterRequest.setStartTime(0);
-        if(bseAlgorithmParameterRequest.getEndTime() < 0) bseAlgorithmParameterRequest.setEndTime(600);
-        if(bseAlgorithmParameterRequest.getSellerRangeFrom() < 0) bseAlgorithmParameterRequest.setSellerRangeFrom(80);
-
-        if(bseAlgorithmParameterRequest.getSellerRangeTo() < 0) {
-            int sellerRangeFrom = bseAlgorithmParameterRequest.getSellerRangeFrom();
-            bseAlgorithmParameterRequest.setSellerRangeTo(sellerRangeFrom < 310 ? 310 : sellerRangeFrom + 100);
-        }
-
-        if(bseAlgorithmParameterRequest.getBuyerRangeFrom() < 0) bseAlgorithmParameterRequest.setBuyerRangeFrom(250);
-
-        if(bseAlgorithmParameterRequest.getBuyerRangeTo() < 0) {
-            int buyerRangeFrom = bseAlgorithmParameterRequest.getBuyerRangeFrom();
-            bseAlgorithmParameterRequest.setSellerRangeTo(buyerRangeFrom < 490 ? 490 : buyerRangeFrom + 100);
-        }
-
-        if(bseAlgorithmParameterRequest.getOrderInterval() < 0) bseAlgorithmParameterRequest.setOrderInterval(30);
-
-        if(bseAlgorithmParameterRequest.getStepMode() == null || "".equals(bseAlgorithmParameterRequest.getStepMode()))
-            bseAlgorithmParameterRequest.setStepMode("fixed");
-        if(bseAlgorithmParameterRequest.getTimeMode() == null || "".equals(bseAlgorithmParameterRequest.getTimeMode()))
-            bseAlgorithmParameterRequest.setTimeMode("periodic");
-    }
 }
--- src/main/java/com/yupi/springbootinit/service/BSEService.java
+++ src/main/java/com/yupi/springbootinit/service/BSEService.java
@@ -1,5 +1,6 @@
 package com.yupi.springbootinit.service;
 
+import com.yupi.springbootinit.constant.BSEConstant;
 import com.yupi.springbootinit.model.dto.bse.BSEAlgorithmParameterRequest;
 import com.yupi.springbootinit.model.py4jInterface.AlgorithmParameterTransfer;
 import org.springframework.stereotype.Service;
@@ -27,6 +28,8 @@ public class BSEService {
     }
 
     public String transferAlgorithmParameter(BSEAlgorithmParameterRequest bseAlgorithmParameterRequest) {
+        validateParameter(bseAlgorithmParameterRequest);
+
         GatewayServer gatewayServer = new GatewayServer();
         gatewayServer.start();
         AlgorithmParameterTransfer entryPoint = (AlgorithmParameterTransfer) gatewayServer.
@@ -39,5 +42,49 @@ public class BSEService {
         }
         gatewayServer.shutdown();
         return callStateString;
+    }
+
+    public void validateParameter(BSEAlgorithmParameterRequest bseAlgorithmParameterRequest) {
+        // default value
+        // start_time 0
+        // end_time 600
+        // seller_range (80, 310)
+        // buyer_range (250, 490)
+        // time_mode periodic
+        // step_mode fixed
+        // order_interval 30
+        if (bseAlgorithmParameterRequest.getStartTime() < 0)
+            bseAlgorithmParameterRequest.setStartTime(BSEConstant.ALGORITHM_START_TIME);
+
+        if (bseAlgorithmParameterRequest.getEndTime() < 0)
+            bseAlgorithmParameterRequest.setEndTime(BSEConstant.ALGORITHM_END_TIME);
+
+        if (bseAlgorithmParameterRequest.getSellerRangeFrom() < 0)
+            bseAlgorithmParameterRequest.setSellerRangeFrom(BSEConstant.SELLER_RANGE_FROM);
+
+        if (bseAlgorithmParameterRequest.getSellerRangeTo() < 0) {
+            int sellerRangeFrom = bseAlgorithmParameterRequest.getSellerRangeFrom();
+            bseAlgorithmParameterRequest.setSellerRangeTo(sellerRangeFrom < BSEConstant.SELLER_RANGE_TO ?
+                    BSEConstant.SELLER_RANGE_TO : sellerRangeFrom + 100);
+        }
+
+        if (bseAlgorithmParameterRequest.getBuyerRangeFrom() < 0)
+            bseAlgorithmParameterRequest.setBuyerRangeFrom(BSEConstant.BUYER_RANGE_FROM);
+
+        if (bseAlgorithmParameterRequest.getBuyerRangeTo() < 0) {
+            int buyerRangeFrom = bseAlgorithmParameterRequest.getBuyerRangeFrom();
+            bseAlgorithmParameterRequest.setSellerRangeTo(buyerRangeFrom < BSEConstant.BUYER_RANGE_TO ?
+                    BSEConstant.BUYER_RANGE_TO : buyerRangeFrom + 100);
+        }
+
+        if (bseAlgorithmParameterRequest.getOrderInterval() < 0)
+            bseAlgorithmParameterRequest.setOrderInterval(BSEConstant.ORDER_INTERVAL);
+
+        if (bseAlgorithmParameterRequest.getStepMode() == null || "".equals(bseAlgorithmParameterRequest.getStepMode()))
+            bseAlgorithmParameterRequest.setStepMode(BSEConstant.STEP_MODE);
+        if (bseAlgorithmParameterRequest.getTimeMode() == null || "".equals(bseAlgorithmParameterRequest.getTimeMode()))
+            bseAlgorithmParameterRequest.setTimeMode(BSEConstant.TIME_MODE);
+
+        // checking the sellers_spec and buyers_spec may conduct at front-end
     }
 }
